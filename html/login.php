d125191207f67e8961af96acc3ad1f75980f513f
youness240 <essabriyouness@outlook.fr>
2022-10-24 13:49:27 +0200

    modif

--- html/login.php
+++ html/login.php
@@ -21,11 +21,11 @@
         session_start();
         if (isset($_POST['username'])){
         $username = stripslashes($_REQUEST['username']);
-        $username = mysqli_real_escape_string($conn, $username);
+        $username = mysqli_real_escape_string($mysqli, $username);
         $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($conn, $password);
+        $password = mysqli_real_escape_string($mysqli, $password);
             $query = "SELECT * FROM `users` WHERE username='$username' and password='".hash('sha256', $password)."'";
-        $result = mysqli_query($conn,$query) or die(mysql_error());
+        $result = mysqli_query($mysqli,$query) or die(mysql_error());
         $rows = mysqli_num_rows($result);
         if($rows==1){
             $_SESSION['username'] = $username;
